59dcbacc9191bc53678d48b76395184678360348
Thomaswalsh1 <130932404+thomaswalsh1@users.noreply.github.com.>
2026-02-14 16:27:23 -0500

    Skills, experience fix, and contact setup

--- src/fileSystem.ts
+++ src/fileSystem.ts
@@ -1,43 +1,39 @@
 export const fileSystem = {
-  '': { // root directory
-    home: {
-      thomas: {
-        'README.txt': 'Welcome to your home directory, thomas!',
-        About: {
-          'Me.txt': 'Here is some quick info about Thomas',
+    '': { // root directory
+        home: {
+            thomas: {
+                'README.txt': 'Welcome to your home directory, thomas!',
+                About: {
+                    'Me.txt': 'Here is some quick info about Thomas',
+                },
+                Experience: {
+                    'Experience.txt': 'Here are some details about my work experience.',
+                },
+                Projects: {
+                    'LinuxLearner.txt': 'Details about LinuxLearner.',
+                    'IBMFullStack.txt': 'Details about my IBM Full Stack Capstone.',
+                    'Vent.txt': 'Details about the Vent App.',
+                    'SuperAI.txt': 'Details about the SuperAI.',
+                },
+                Education: {
+                    'Education.txt': 'Education details.',
+                },
+                Blog: {
+                    '2_14_26.txt': 'Blog post for Feb 14th.',
+                },
+                Skills: {
+                    'Skills.txt': 'A summary of my skills and libraries',
+                },
+                Contact: {
+                    'Contact.txt': 'Contact information.',
+                },
+            },
         },
-        Experience: {
-          'Experience.txt': 'Here are some details about my work experience.',
+        etc: {},
+        bin: {
+            'echo': '#!/bin/sh\necho $@',
+            'ls': '#!/bin/sh\nls $@',
         },
-        Projects: {
-          'LinuxLearner.txt': 'Details about LinuxLearner.',
-          'IBMFullStack.txt': 'Details about my IBM Full Stack Capstone.',
-          'Vent.txt': 'Details about the Vent App.',
-          'SuperAI.txt': 'Details about the SuperAI.',
-        },
-        Education: {
-          'Education.txt': 'Education details.',
-        },
-        Blog: {
-          '2_14_26.txt': 'Blog post for Feb 14th.',
-        },
-        Skills: {
-          'Frontend.txt': 'Frontend skills.',
-          'Backend.txt': 'Backend skills.',
-          'Cloud.txt': 'Cloud skills.',
-          'IT.txt': 'IT skills.',
-          'Other.txt': 'Other skills.',
-        },
-        Contact: {
-          'Info.txt': 'Contact information.',
-        },
-      },
-    },
-    etc: {},
-    bin: {
-      'echo': '#!/bin/sh\necho $@',
-      'ls': '#!/bin/sh\nls $@',
-    },
-    tmp: {},
-  }
+        tmp: {},
+    }
 };
