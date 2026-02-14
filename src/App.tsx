0f99429b9f4ef40f4a52ef28d52e8ec40f8b1f08
Thomaswalsh1 <130932404+thomaswalsh1@users.noreply.github.com.>
2026-02-14 13:55:54 -0500

    Vite + Xterm initialization

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Welcome to my terminal app!</h1>
+        <p>by Tom</p>
+      </div>
+    </>
+  )
+}
+
+export default App
